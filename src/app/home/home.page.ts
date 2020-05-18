01002e4a1d5ffa0067a5ee3d96dc59e9903d6c77
kmorguen <62434860+kmorguen@users.noreply.github.com>
2020-05-18 01:08:04 +0000

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public showCamera = false;
+  public textScanned: string = '';
 
-  constructor() {}
+  constructor( 
+    private qrScanner: QRScanner,
+    private iab: InAppBrowser,) {
+    this.scanCode();
+  }
 
+  scanCode() {
+    this.showCamera = true;
+    // Optionally request the permission early
+    this.qrScanner.prepare()
+    .then((status: QRScannerStatus) => {
+      if (status.authorized) {
+        // start scanning
+        console.log('Scan en cours...' + JSON.stringify(status));
+        const scanSub = this.qrScanner.scan().subscribe((text: any) => {
+          console.log('Scanned something', text.result);
+          this.textScanned = text.result;
+          this.qrScanner.hide(); // hide camera preview
+          scanSub.unsubscribe(); // stop scanning
+          this.showCamera = false;
+        });
+      } else if (status.denied) {
+        // camera permission was permanently denied
+      } else {
+        // permission was denied, but not permanently. You can ask for permission again at a later time.
+      }
+    })
+    .catch((e: any) => console.log('Error is', e));
+  }
+
+  closeCamera() {
+    this.showCamera = false;
+    this.qrScanner.hide(); // hide camera preview
+    this.qrScanner.destroy();
+  }
+
+  openLink(link: any) {
+    const browser = this.iab.create(link, '_system', 'location=yes');
+  }
 }
